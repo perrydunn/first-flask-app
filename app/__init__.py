66b21e7ee70c51030517b8afc0e3892ca8d4407b
Peregrine Dunn <perrydunn@hotmail.co.uk>
2018-12-27 18:48:22 +0000

    AMENDED Ch.11-12 inclusive - skipping Ch.13

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,6 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_babel import Babel
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,7 +19,6 @@ login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-babel = Babel(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -48,9 +46,5 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
-
-@babel.localeselector
-def get_locale():
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 from app import routes, models, errors
